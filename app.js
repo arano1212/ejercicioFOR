cf0f6aaaad930d22947e34e6365140d8467ba70a
arano1212 <angelgonzalezarano@MacBook-Air-de-Angel.local>
2023-08-30 22:55:19 -0600

    ejercicio4

---
+++ app.js
@@ -0,0 +1,107 @@
+//ejercio 4
+//FOR
+//NUMERO 1
+
+/*(for (let num = 1; num <=50; num++) {
+    if (num%2===1) {
+
+        console.log(num);
+        
+    }
+    
+    
+}*/
+
+//numero 2 
+/*let pokemonlist = [
+    { number: 1, name: 'bulbasur' },
+    { number: 2, name: 'ivysaur' },
+    { number: 3, name: 'venausur' },
+    { number: 4, name: 'chamander' },
+    { number: 5, name: 'charmeleon' },
+    { number: 6, name: 'charizard' },
+    { number: 7, name: 'squirtle' },
+    { number: 8, name: 'wartotle' },
+    { number: 9, name: 'blastoise' },
+    { number: 10, name: 'caterpie' },
+    { number: 11, name: 'metapod' },
+    { number: 12, name: 'butterfree' },
+    { number: 13, name: 'weedle' },
+    { number: 14, name: 'kakuna' },
+    { number: 15, name: 'beedrill' },
+    { number: 16, name: 'pidgey' },
+    { number: 17, name: 'pidgeotto' },
+    { number: 18, name: 'pidgeot' },
+    { number: 19, name: 'rattata' },
+    { number: 20, name: 'raticate' },
+    { number: 21, name: 'spearow' },
+    { number: 22, name: 'fearow' },
+    { number: 23, name: 'ekans' },
+    { number: 24, name: 'arbok' },
+    {number: 25, name: 'pikachu'},
+    {number:26, name:'raichu'},
+    {number:27, name: 'sandshrew'},
+    { number: 28, name: 'nidoran' },
+    { number: 29, name: 'nidorina' },
+    { number: 30, name: 'nidoqueem' },
+    { number: 31, name: 'nidoran' },
+    { number: 32, name: 'nidorino' },
+    { number: 33, name: 'nidoking' },
+    { number: 34, name: 'cleafary' },
+    { number: 35, name: 'clefable' },
+    { number: 36, name: 'vulpix' },
+    { number: 37, name: 'ninetales' },
+    { number: 38, name: 'jigglypuff' },
+    { number: 39, name: 'wigglypuff' },
+    { number: 40, name: 'zubat' },
+    { number: 41, name: 'golbat' },
+    { number: 42, name: 'oddish' },
+    { number: 43, name: 'gloom' },
+    { number: 44, name: 'vileplume' },
+    { number: 45, name: 'paras' },
+    { number: 46, name: 'parasect' },
+    { number: 47, name: 'venonat' },
+    { number: 48, name: 'diglett' },
+    { number: 49, name: 'dugtrio' },
+    { number: 50, name: 'meowth' },
+    { number: 51, name: 'persian' },
+    {number: 53, name: 'psyduck'},
+    { number: 54, name: 'golduck' },
+    { number: 55, name: 'mankey' },
+    { number: 56, name: 'primeape' },
+    { number: 57, name: 'growlithe' },
+    { number: 58, name: 'arcanine' },
+    { number: 59, name: 'poliwag' },
+    { number: 60, name: 'poliwhirl' },
+    {number:61, name:'poliwrat'},
+    
+];
+
+let userNumber = parseFloat(prompt('coloca un numero de pokemon'));
+
+for (let i = 0; i < pokemonlist.length; i++) {
+    if (userNumber%5===0 && userNumber <= pokemonlist.length) {
+        console.log(pokemonlist[userNumber - 1].name)   
+        
+       } else {
+           console.log('pokemon no existe o no es multiplo de 5 ');
+       }
+    
+}*/
+
+
+
+
+//numero 3
+/*let recorrido = [ 4, "dos", 8, "tres", 5, 9, 1, "cero"];
+
+for (let i = 0; i < recorrido.length; i++) {
+    if (typeof recorrido[i] === "number") {
+        console.log(recorrido[i]);
+    }
+    
+}*/
+
+
+
+
